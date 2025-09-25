2ce71abe0376ed1176d553318803ac0c2525b6a0
JosueGarcia04 <josueadrian042006@gmail.com>
2025-09-24 23:27:05 -0600

    first commit

---
+++ src/main/java/sv/edu/udb/Investigacion/InvestigacionAplicada2Application.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.Investigacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestigacionAplicada2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestigacionAplicada2Application.class, args);
+	}
+
+}
